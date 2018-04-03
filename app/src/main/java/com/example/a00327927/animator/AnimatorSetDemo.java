6271ed19d93e6f14662b4e51d48a055c05e60aef
cm <che_min@want-want.com>
2018-04-03 16:38:08 +0800

    fix(customview):优化自定弹义menu

--- app/src/main/java/com/example/a00327927/animator/AnimatorSetDemo.java
+++ app/src/main/java/com/example/a00327927/animator/AnimatorSetDemo.java
@@ -18,7 +18,9 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
 import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
 
 import com.example.a00327927.customview.R;
 
@@ -33,7 +35,7 @@ import java.util.List;
 
 public class AnimatorSetDemo extends View {
 
-    private int mCenterX, mCenterY, mWidth, mHeight;
+    private int mCenterX, mCenterY;
     private float radius;//伸缩半径
     private int picWidth = 60;//图片宽
     private int picHeight = 60;//图片宽
@@ -45,13 +47,14 @@ public class AnimatorSetDemo extends View {
     private Bitmap mBmGit;
     private Bitmap mBmMenu;
     private List<Bitmap> mBitmaps;
-    private boolean isKeepAway = false;
+    private boolean isKeepAway = false;//是否是远离菜单按钮
     private float mStartLengh;
-    private float mStartHeight;
     private float mEndValue;
     private double degree;
     private final String TAG = AnimatorSetDemo.class.getSimpleName();
     private boolean isFirstDraw = true;
+    private float mFraction;
+    private int maxWidth;//自定义view最大宽度
 
     public AnimatorSetDemo(Context context) {
         this(context, null);
@@ -97,6 +100,11 @@ public class AnimatorSetDemo extends View {
         degree = Math.PI / 2 / (mBitmaps.size()-2);
         Log.i(TAG, "degree---" + degree);
 
+        //初始化移动最大距离
+        mEndValue = radius;
+        //初始化最大边距
+        maxWidth=picWidth;
+
     }
 
     @Override
@@ -111,7 +119,7 @@ public class AnimatorSetDemo extends View {
 
         switch (widthMode) {
             case MeasureSpec.AT_MOST:
-                currWidth = (int) (radius + picWidth);
+                currWidth =maxWidth;
                 break;
             case MeasureSpec.EXACTLY:
                 currWidth = width;
@@ -123,7 +131,7 @@ public class AnimatorSetDemo extends View {
 
         switch (heightMode) {
             case MeasureSpec.AT_MOST:
-                currHeight = (int) (radius + picWidth);
+                currHeight =maxWidth;
                 break;
             case MeasureSpec.EXACTLY:
                 currHeight = height;
@@ -135,36 +143,42 @@ public class AnimatorSetDemo extends View {
         setMeasuredDimension(currWidth, currHeight);
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        mWidth = w;
-        mHeight = h;
-        mEndValue = mWidth - picWidth;
-
-    }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         double rad = 0;
-        canvas.translate(radius,radius);
-        if (isFirstDraw) {
+        if (isFirstDraw) {//第一次绘制
+//            canvas.translate(picWidth,picWidth);
             isFirstDraw = false;
-            for (int i = 0; i < mBitmaps.size() - 1; i++) {
+            mPaint.setAlpha(0);//先隐藏除菜单图片外的所有图片
+            for (int i = 0; i < mBitmaps.size() - 1; i++) {//绘制每个图片
                 canvas.drawBitmap(mBitmaps.get(i), 0, 0, mPaint);
             }
+            mPaint.setAlpha(255);
+            canvas.drawBitmap(mBitmaps.get(mBitmaps.size() - 1),0, 0, mPaint);
         } else {
+            canvas.translate(maxWidth-picWidth,maxWidth-picWidth);
+            if (isKeepAway){
+                mPaint.setAlpha((int) (255*mFraction));
+            }else {
+                mPaint.setAlpha((int) (255*(1-mFraction)));
+            }
+
             for (int i = 0; i < mBitmaps.size() - 1; i++) {
                 int x =-(int) (mStartLengh * Math.cos(rad));
                 int y =-(int) (mStartLengh * Math.sin(rad));
                 canvas.drawBitmap(mBitmaps.get(i), x, y, mPaint);
                 rad += degree;
-                Log.e(TAG, "rad--" + rad + ",x--" + x + ",y--" + y);
+//                Log.e(TAG, "rad--" + rad + ",x--" + x + ",y--" + y);
             }
+
+            mPaint.setAlpha(255);
+            canvas.drawBitmap(mBitmaps.get(mBitmaps.size() - 1),0, 0, mPaint);
         }
-        canvas.drawBitmap(mBitmaps.get(mBitmaps.size() - 1),0, 0, mPaint);
+
+
     }
 
 
@@ -182,16 +196,20 @@ public class AnimatorSetDemo extends View {
 
     private void doAnimator(float startValue, float endValue) {
 
+
+
         ValueAnimator valueAnimator = ValueAnimator.ofFloat(startValue, endValue);
-        Log.e("cm", "start---" + mStartLengh + ",end--" + radius);
-        valueAnimator.setDuration(300);
-        valueAnimator.setInterpolator(new AccelerateInterpolator());
+//        Log.e("cm", "start---" + mStartLengh + ",end--" + radius);
+        valueAnimator.setDuration(800);
+        valueAnimator.setInterpolator(new BounceInterpolator());
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 mStartLengh = (float) animation.getAnimatedValue();
-                invalidate();
-                Log.e("cm", "invalidate---->" + mStartLengh);
+                mFraction = animation.getAnimatedFraction();
+                maxWidth= (int) (mStartLengh+picWidth);
+                requestLayout();
+                Log.e("cm", "invalidate---->" + mStartLengh+",fraction:"+ mFraction+",maxWidth"+maxWidth);
 
             }
         });
@@ -210,6 +228,17 @@ public class AnimatorSetDemo extends View {
             }
         });
         valueAnimator.start();
+    }
+
+    /**
+     * 回收资源
+     */
+    public void recyclerResource(){
+        if (mBitmaps!=null){
+            for (int i = 0; i < mBitmaps.size(); i++) {
+                mBitmaps.get(i).recycle();
+            }
+        }
     }
 
 }
